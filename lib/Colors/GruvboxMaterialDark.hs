f908e52cddba709273474b9b1270e8d2268719cf
Ether <ahanafether@gmail.com>
2022-07-14 10:55:57 -0500

    Clean up

--- lib/Colors/GruvboxMaterialDark.hs
+++ lib/Colors/GruvboxMaterialDark.hs
@@ -1,3 +1,6 @@
+-- Gruvbox Material Dark
+-- https://github.com/sainnhe/gruvbox-material/blob/master/autoload/gruvbox_material.vim
+
 module Colors.GruvboxMaterialDark where
 
 import XMonad
@@ -11,18 +14,42 @@ colorBg3 = "#3c3836"
 colorBg5 = "#504945"
 colorFg = "#e2cca9"
 
+-- Red
 colorRed = "#f2594b"
+colorBgRed = "#db4740"
+colorBgDRed = "3c1f1e"
+colorBgVRed = "#3c1f1e"
+
+-- Orange
 colorOrange = "#f28534"
+
+-- Yellow
 colorYellow = "#e9b143"
+colorBgYellow = "#e9b143"
+colorBgVYellow = "#473c29"
+
+-- Green
 colorGreen = "#b0b846"
+colorBgGreen = "#b0b846"
+colorBgDGreen = "#32361a"
+colorBgVGreen = "#333e34"
+
+-- Aqua
 colorAqua = "#8bba7f"
+
+-- Blue
 colorBlue = "#80aa9e"
+colorBgVBlue = "#2e3b3b"
+colorBgDBlue = "#0d3138"
+
+-- Purple
 colorPurple = "#d3869b"
 
-colorbgRed = "#db4740"
-
+-- Grey
 colorGrey0 = "7c6f64"  
 colorGrey1 = "#928374"
 colorGrey2 = "#a89984"
 
 colorTrayer = "--tint 0x1d2021"
+
+-- vim: ft=haskell:foldmethod=marker:expandtab:ts=4:shiftwidth=4
