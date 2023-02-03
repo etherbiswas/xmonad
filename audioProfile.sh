50549d962a176de3efb79bf53bbc5b1d62e3c94c
Ether <etherrorcode404@gmail.com>
2023-02-04 02:07:32 +0600

    Update audioProfile.sh

--- audioProfile.sh
+++ audioProfile.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+
+# ensure only one instance of script is running
+if pidof -x $(basename $0) -o %PPID > /dev/null
+then
+    exit
+fi
+
 # Pc-Jack
-pacmd set-card-profile alsa_card.pci-0000_09_00.1 off 
+pacmd set-card-profile alsa_card.pci-0000_09_00.1 off
 # Pc-Jack-2
-pacmd set-card-profile alsa_card.pci-0000_09_00.6 off 
+pacmd set-card-profile alsa_card.pci-0000_09_00.6 off
 # WebCam
-pacmd set-card-profile alsa_card.usb-Sonix_Technology_Co.__Ltd._Rapoo_Camera_SN0001-02 off 
+pacmd set-card-profile alsa_card.usb-Sonix_Technology_Co.__Ltd._Rapoo_Camera_SN0001-02 off
 # Usb-Headset
-pacmd set-card-profile alsa_card.usb-GeneralPlus_USB_Audio_Device-00 output:analog-stereo+input:mono-fallback 
+pacmd set-card-profile alsa_card.usb-GeneralPlus_USB_Audio_Device-00 output:analog-stereo+input:mono-fallback
